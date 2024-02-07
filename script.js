7634c37e7aabaedb5a43c59f0c9e6255a1b8121f
Mika Hughes <70777974+mikaaurora@users.noreply.github.com>
2024-02-06 20:59:24 -0500

    Refactor to separate files & smooth out transition

---
+++ script.js
@@ -0,0 +1,41 @@
+var positionX = 0;
+var velocity = 100;
+var height = 80;
+var width = 80;
+var lineHeight = 80;
+var fontSize = 12; // Initial font size for "breathe in" text
+var reverse = false;
+var ball = document.getElementById('ball');
+
+function moveBall() {
+  var Xmin = 0;
+  var Xmax = 300;
+
+  if (reverse) {
+    positionX -= velocity;
+    height /= 1.25;
+    width /= 1.25;
+    lineHeight /= 1.25;
+    fontSize /= 1.25; // Decrease font size
+    ball.innerHTML = 'breathe out';
+  } else {
+    positionX += velocity;
+    height *= 1.25;
+    width *= 1.25;
+    lineHeight *= 1.25;
+    fontSize *= 1.25; // Increase font size
+    ball.innerHTML = 'breathe in';
+  }
+
+  ball.style.left = positionX + 'px';
+  ball.style.height = height + 'px';
+  ball.style.width = width + 'px';
+  ball.style.lineHeight = lineHeight + 'px';
+  ball.style.fontSize = fontSize + 'px'; // Apply the updated font size
+
+  if (positionX > Xmax || positionX === Xmin) {
+    reverse = !reverse;
+  }
+}
+
+setInterval(moveBall, 1500);
